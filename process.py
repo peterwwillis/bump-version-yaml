5062466c8ea97ae2f9669992bd63b7f54809900b
Peter Willis <peterwwillis@gmail.com>
2018-12-14 01:11:54 -0500

    Fix map.yaml, put back leading 'v', set values in dataset

--- process.py
+++ process.py
@@ -10,16 +10,13 @@ from versio.version import Version
 from versio.version_scheme import Pep440VersionScheme, Simple3VersionScheme, Simple4VersionScheme, PerlVersionScheme, \
             Simple5VersionScheme, VariableDottedIntegerVersionScheme
 
-
-YAML_FILE = "example.yaml"
-MAP_FILE = "map.yaml"
-
 class Log(object):
     logging = True
     def __init__(self, string, i=0, logging=True):
         if self.logging == False or logging == False: return
         print( (" " * (i*4)) + string )
 class Debug(Log):
+    """ Debug class. To enable, set logging = True """
     logging = False
 
 # Look in versio/version_scheme.py to see how to extend this with new
@@ -27,14 +24,13 @@ class Debug(Log):
 # existing schemas.
 # 
 # In the default schema (PEP440), a version is made up of "fields", the
-# default of which is the "release" field, like "1.2.3" in "1.2.3rc1".
-# Then there are "subfields" of the field, which would be "major" ("1"),
-# "minor" ("2"), and "tiny" ("3").
+# default of which is the "release" field (like "4.5.6" in "4.5.6rc1").
+# Then there are "subfields" of the field, which would be "major" ("4"),
+# "minor" ("5"), and "tiny" ("6").
 # 
 # Trying to bump a version from "1.2.3rc" to "1.2.3" will require an extra
 # promote=True option to bump, and we'll probably need to support an extra
 # mapping arg for this. #TODO
-
 class SemanticVersion(object):
     scheme = None
     def __init__(self, bump):
@@ -42,8 +38,7 @@ class SemanticVersion(object):
         # Defaulting to the Pep440 version scheme for this class
         self.scheme = Pep440VersionScheme
     def do(self, ver):
-        # Bump version
-        Debug("self.bump: %s" % self.bump)
+        Debug("do(%s) self.bump: %s" % (ver, self.bump))
 
         # If bump is a list of bumps to perform, create a new class instance
         # and set its bump, do the bump, then return result
@@ -55,7 +50,7 @@ class SemanticVersion(object):
                 Debug("Running v2.do(%s)" % ver)
                 ver = v2.do( str(ver) )
                 Debug("v2.do() result: %s" % ver)
-            return
+            return str(ver)
 
         myver = Version(ver, scheme=self.scheme)
         myver.bump(self.bump)
@@ -70,30 +65,32 @@ class ForceSemanticVer(SemanticVersion):
     def do(self, ver):
         Debug("do(%s)" % ver)
         # Clean up version string
-        #ver.strip()
-        regex = re.compile(r"^v", re.IGNORECASE)
-        Debug("regex '%s' ver '%s' ver type '%s'" % (regex, ver, type(ver)))
-        match = regex.search(ver)
-        if match:
+        stripver = re.search(r"^v", ver, re.IGNORECASE)
+        if stripver:
             Debug("Found and replaced a leading 'v' in version string")
-            ver = regex.sub("", ver)
-        return super().do(ver)
+            ver = re.sub(r"^v", "", ver)
+        ver = super().do(ver)
+        Debug("got %s from super().do" % ver)
+        if stripver:
+            ver = "v" + ver
+            Debug("Put the leading 'v' back on version")
+        return ver
+
 
 class BumpVers(object):
-    y = {}
-    logging = True
+    data = {}
 
     def load_f(self, name, path):
         #yaml = YAML(typ='unsafe')
         yaml = YAML()
         yaml.register_class(SemanticVersion)
         yaml.register_class(ForceSemanticVer)
-        self.y[name] = yaml.load(path)
+        self.data[name] = yaml.load(path)
 
     def walk_data(self, m, d, i=0):
-        Debug("\n")
-        Debug("d: '%s', type: '%s'" % (d, type(d)), i)
-        Debug("m: '%s', type: '%s'" % (m, type(m)), i)
+        #Debug("\n")
+        #Debug("d: '%s', type: '%s'" % (d, type(d)), i)
+        #Debug("m: '%s', type: '%s'" % (m, type(m)), i)
 
         if isinstance(d, dict):
             for key in d.keys():
@@ -101,21 +98,29 @@ class BumpVers(object):
                 for mkey in m.keys():
                     if key == mkey:
                         Debug("Found key '%s' matching map key" % key, i)
-                        self.walk_data( m[mkey], d[key], i+1 )
+                        newm, newd = self.walk_data( m[mkey], d[key], i+1 )
+                        # Replace the old value with the new one
+                        Debug("oldd\n%s\nnewd\n%s\n" % (d[key], newd))
+                        d[key] = newd
         elif isinstance(d, list) or isinstance(d, tuple):
+            #TODO this code is not finished
             raise Exception("TODO: please finish this code path")
-            for item in d:
+            for c, item in enumerate(d):
                 Debug("d item '%s'" % key, i)
                 #for mitem in m:
-                self.walk_data( m, item, i+1 )
+                newm, newd = self.walk_data( m, item, i+1 )
+                d[c] = newd
         else:
             Debug("d entry: '%s', type: '%s'" % (d, type(d)), i)
             if isinstance(m, SemanticVersion):
                 Debug("found SemanticVersion", i)
                 d = m.do(d)
+                Debug("d result: %s" % d)
+
+        return m, d
 
     def bump_vers(self):
-        self.walk_data( self.y["map"], self.y["data"] )
+        self.walk_data( self.data["map"], self.data["data"] )
 
 def options():
     parser = argparse.ArgumentParser(description='Bump version strings in YAML files')
